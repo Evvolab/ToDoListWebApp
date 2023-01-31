dbf2582a2154d4d13d1b49ec55bd6812794d25b4
midhunmohank <mohan.ku@northeastern.edu>
2023-01-31 02:56:23 -0500

    background added

--- functions.py
+++ functions.py
@@ -1,10 +1,10 @@
-def get_todos(filepath='todos.txt'):
+def get_todos(filepath='files/todos.txt'):
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(todos_arg, filepath='todos.txt'):
+def write_todos(todos_arg, filepath='files/todos.txt'):
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_arg)
 
--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 # Imports
 import streamlit as st
+import base64
 import functions
 
 def add_todo():
@@ -25,5 +26,27 @@ for item in todos:
 
 st.text_input(label = 'input box', label_visibility='hidden', placeholder='Add new to-do',
         on_change= add_todo, key= 'new_todo')
+
+
+
+# Background
+
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+
+    
+add_bg_from_local('files/background.jpg') 
 
 
